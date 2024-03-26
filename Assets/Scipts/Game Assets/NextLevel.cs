7304555cda817dabb36c98dd1ff50e249ee433b5
640466057 <135637046+640466057@users.noreply.github.com>
2024-03-26 23:07:53 +0100

    Exit door texture

--- Assets/Scipts/Game Assets/NextLevel.cs
+++ Assets/Scipts/Game Assets/NextLevel.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    [SerializeField] private int keysRequired;
-    [SerializeField] private GameObject player;
+    [SerializeField] Animator anim;
+    [SerializeField] int keysRequired;
+    [SerializeField] GameObject player;
+    bool open = false;
 
     void Update()
     {
-        
+        if (player.TryGetComponent(out PlayerController PC) && PC.KeysObtained >= keysRequired && !open)
+        {
+            anim.SetTrigger("Open");
+            open = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
