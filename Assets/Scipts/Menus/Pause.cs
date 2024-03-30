9598cc59d3c96bb56d15a0f3490bc4987038d239
640466057 <135637046+640466057@users.noreply.github.com>
2024-03-30 12:09:19 +0100

    Pause Menu Fix
    The player was able to give inputs whilst being paused, that would then get executed as soon as the game was unpaused

--- Assets/Scipts/Menus/Pause.cs
+++ Assets/Scipts/Menus/Pause.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = player.GetComponent<PlayerController>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && TryGetComponent(out Canvas pauseMenu))
@@ -12,11 +20,13 @@ public class Pause : MonoBehaviour
             {
                 pauseMenu.enabled = false;
                 Time.timeScale = 1;
+                playerController.enabled = true;
             }
             else if (pauseMenu.enabled == false)
             {
                 pauseMenu.enabled = true;
                 Time.timeScale = 0;
+                playerController.enabled = false;
             }
         }
     }
