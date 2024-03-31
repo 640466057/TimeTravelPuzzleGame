985ecb98de0ba508c5fff51b32edaeefe9b29ded
640466057 <135637046+640466057@users.noreply.github.com>
2024-03-31 17:30:55 +0200

    Fade
    Added a fade effect between levels

--- Assets/Scipts/Game Assets/NextLevel.cs
+++ Assets/Scipts/Game Assets/NextLevel.cs
@@ -8,7 +8,17 @@ public class NextLevel : MonoBehaviour
     [SerializeField] Animator anim;
     [SerializeField] int keysRequired;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject fade;
+    Animator fadeAnim;
     bool open = false;
+
+    private void Start()
+    {
+        if (fade != null && fade.transform.childCount != 0 && fade.transform.GetChild(0).TryGetComponent(out Animator fadeAnim))
+        {
+            this.fadeAnim = fadeAnim;
+        }
+    }
 
     void Update()
     {
@@ -23,7 +33,10 @@ public class NextLevel : MonoBehaviour
     {
         if (player.TryGetComponent(out PlayerController PC) && PC.KeysObtained >= keysRequired)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            if (fadeAnim != null)
+                fadeAnim.SetTrigger("FadeOut");
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
